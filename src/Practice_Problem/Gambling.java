af28a82d12e7ba25358d83a4b07f3a5ab9bd8e50
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-10-31 11:46:36 +0530

    Gambler_Everday_stack&bet

---
+++ src/Practice_Problem/Gambling.java
@@ -0,0 +1,9 @@
+package Practice_Problem;
+
+public class Gambling {
+public static void main(String[] args) {
+	int Everyday_stack = 100;
+	int bet_amount = 1;
+	System.out.println("Everyday stack $"+Everyday_stack+";"+" Bet amount $"+bet_amount);
+  }
+}
